109923882c6cdc11d1e209f08c8605c6e885b0dd
simon04PL <szymon.msa@gmail.com>
2025-08-10 19:52:45 +0000

    Update formula with new release

--- dml.rb
+++ dml.rb
@@ -1,9 +1,9 @@
 class Dml < Formula
   desc "Custom DSL for data definition like JSON, with Kotlin & ANTLR"
   homepage "https://github.com/tree-software-company/DML"
-  url "https://github.com/tree-software-company/DML/releases/download/0.4.2/DML-all.jar"
-  version "0.4.2"
-  sha256 "17f8cb7592b39657d0233babcd42ec3594ebbcf92ab25458b7e0f1a59dd81b4a"
+  url "https://github.com/tree-software-company/DML/releases/download/0.5.0/DML-all.jar"
+  version "0.5.0"
+  sha256 "642dc7452057612840beceb8dc72c39d228669c61ff6ade6b8a8886b912f8f2a"
   license "Apache-2.0"
 
   def install
