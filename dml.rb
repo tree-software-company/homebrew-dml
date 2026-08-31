8b71307e4d29b23f145b05c31eb469a187193e8f
simon04PL <szymon.msa@gmail.com>
2026-08-31 20:29:35 +0000

    Update formula with new release

--- dml.rb
+++ dml.rb
@@ -3,7 +3,7 @@ class Dml < Formula
   homepage "https://github.com/tree-software-company/DML"
   url "https://github.com/tree-software-company/DML/releases/download/0.8.0/DML-all.jar"
   version "0.8.0"
-  sha256 "252be779807d255e152ed0e14c19718c1e1854d12f3414a9f41ea9266c729434"
+  sha256 "04959ad5c6b4487dbbd6ecb9a1c14cc794e7754e348cec7c8b3b4e3b1ea80de1"
   license "Apache-2.0"
 
   def install
