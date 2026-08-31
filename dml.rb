9ce9a394c7580e38f7d6e0cbd3815ff0000b109e
simon04PL <szymon.msa@gmail.com>
2026-08-31 22:10:35 +0200

    Fix sha256 for 0.8.0 DML-all.jar release asset

--- dml.rb
+++ dml.rb
@@ -3,7 +3,7 @@ class Dml < Formula
   homepage "https://github.com/tree-software-company/DML"
   url "https://github.com/tree-software-company/DML/releases/download/0.8.0/DML-all.jar"
   version "0.8.0"
-  sha256 "8daae3d35b6534bdb6fd7f7a003c24410db38a59a9c25a0dfd2fe00d2908f73f"
+  sha256 "252be779807d255e152ed0e14c19718c1e1854d12f3414a9f41ea9266c729434"
   license "Apache-2.0"
 
   def install
